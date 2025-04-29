deca9745cc609e0d3252e952ab314990b85df414
Vinod Kumawat <102738062+Vinod9799@users.noreply.github.com>
2025-04-29 23:31:05 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-
+////
 
 Route::group(['prefix' => 'RkVkcizer', 'as' => 'admin.'], function () {
     Route::middleware('auth')->group(function () {
