4888f3be3c36d8519ea9a5e0985f7ec08a7764ef
Vinod kumar kumawat <vinodkumawat20052002@gmail.com>
2025-04-29 23:25:40 +0530

    route in add seller prefix

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,19 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-    
+
 
 Route::group(['prefix' => 'RkVkcizer', 'as' => 'admin.'], function () {
+    Route::middleware('auth')->group(function () {
+        Route::get('dashboard', DashboardComponents::class)
+            ->name('dashboard');
+        Route::get('users', UserComponents::class)
+            ->name('user');
+        Route::get('role-permission', RolePermissionComponents::class)
+            ->name('role-permission');
+    });
+});
+Route::group(['prefix' => 'seller'], function () {
     Route::middleware('auth')->group(function () {
         Route::get('dashboard', DashboardComponents::class)
             ->name('dashboard');
